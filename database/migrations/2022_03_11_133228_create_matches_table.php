29c17c2698ddb807e10045ce7bcec4587cd815e9
fakhriahmadov <fakhriahmedoff@gmail.com>
2022-03-11 18:42:32 +0400

    first

---
+++ database/migrations/2022_03_11_133228_create_matches_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_one_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreignId('user_two_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+};
